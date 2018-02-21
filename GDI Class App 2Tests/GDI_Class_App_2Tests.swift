8da7f87c4ec918fbe7213ff638c5c3b373f480cc
lilybarrett <lily@barrett.us>
2018-02-20 19:41:55 -0500

    Initial Commit

---
+++ GDI Class App 2Tests/GDI_Class_App_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GDI_Class_App_2Tests.swift
+//  GDI Class App 2Tests
+//
+//  Created by Lily Barrett1 on 2/20/18.
+//  Copyright © 2018 Lily Barrett1. All rights reserved.
+//
+
+import XCTest
+@testable import GDI_Class_App_2
+
+class GDI_Class_App_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
